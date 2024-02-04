830448aeccc8487ec9113f4cc60cc2c0feceedac
hyejj19@naver.com <hyejj19@naver.com>
2024-02-04 15:00:22 +0900

    JSON 반환하기

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,26 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
+interface Post {
+  author: string;
+  title: string;
+  content: string;
+  likeCount: number;
+  commentCount: number;
+}
+
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getPost(): Post {
+    return {
+      author: 'newjeans_official',
+      title: '뉴진스 민지',
+      content: '민지는 예쁘다',
+      likeCount: 10000000,
+      commentCount: 99999,
+    };
   }
 }
