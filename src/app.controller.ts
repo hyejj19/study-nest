209988003c8af9854f94a640d8620c651a35a30c
hyejj19@naver.com <hyejj19@naver.com>
2024-02-04 15:29:30 +0900

    Posts 모듈 생성하기

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,26 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { AppService } from './app.service';
-
-interface Post {
-  author: string;
-  title: string;
-  content: string;
-  likeCount: number;
-  commentCount: number;
-}
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getPost(): Post {
-    return {
-      author: 'newjeans_official',
-      title: '뉴진스 민지',
-      content: '민지는 예쁘다',
-      likeCount: 10000000,
-      commentCount: 99999,
-    };
-  }
 }
---
+++ src/posts/posts.controller.ts
@@ -0,0 +1,26 @@
+import { Controller, Get } from '@nestjs/common';
+import { PostsService } from './posts.service';
+
+interface Post {
+  author: string;
+  title: string;
+  content: string;
+  likeCount: number;
+  commentCount: number;
+}
+
+@Controller('posts')
+export class PostsController {
+  constructor(private readonly postsService: PostsService) {}
+
+  @Get()
+  getPost(): Post {
+    return {
+      author: 'newjeans_official',
+      title: '뉴진스 민지',
+      content: '민지는 예쁘다',
+      likeCount: 10000000,
+      commentCount: 99999,
+    };
+  }
+}
