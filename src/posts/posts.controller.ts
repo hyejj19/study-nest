388ebb01bdae181d5391bbecc0322256d2dc4b29
hyejj19@naver.com <hyejj19@naver.com>
2024-02-04 16:06:53 +0900

    기본적인 CRUD 엔드포인트 생성

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,7 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { PostsService } from './posts.service';
 
-interface Post {
+interface PostModel {
+  id: number;
   author: string;
   title: string;
   content: string;
@@ -9,18 +18,90 @@ interface Post {
   commentCount: number;
 }
 
+let posts: PostModel[] = [
+  {
+    id: 1,
+    author: 'newjeans_official',
+    title: '뉴진스 민지',
+    content: '민지는 예쁘다',
+    likeCount: 10000000,
+    commentCount: 99999,
+  },
+  {
+    id: 2,
+    author: 'newjeans_official',
+    title: '해린',
+    content: '해린은 이쁘다',
+    likeCount: 10000000,
+    commentCount: 99999,
+  },
+  {
+    id: 3,
+    author: 'newjeans_official',
+    title: '다니엘',
+    content: '다니엘은 이쁘다',
+    likeCount: 10000000,
+    commentCount: 99999,
+  },
+];
+
+/**
+ * GET /posts
+ * 모든 post 를 다 가져온다.
+ 
+ * GET /posts/:id
+ * id 에 해당되는 post 르 가져온다.
+ 
+ * POST /posts
+ * post 를 생성한다.
+ 
+ * PUT /posts/:id
+ * id 에 해당하는 post 를 변경한다.
+ 
+ * DELETE /posts/:id
+ * id 에 해당하는 post 를 삭제한다.
+ */
+
 @Controller('posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
 
   @Get()
-  getPost(): Post {
-    return {
-      author: 'newjeans_official',
-      title: '뉴진스 민지',
-      content: '민지는 예쁘다',
-      likeCount: 10000000,
-      commentCount: 99999,
-    };
+  getPosts(): PostModel[] {
+    return posts;
+  }
+
+  @Get(':id')
+  getPost(@Param('id') postId: string): PostModel {
+    return posts.find((post) => post.id === +postId);
+  }
+
+  @Post()
+  create(@Body() postData: PostModel): PostModel {
+    posts.push({
+      id: posts.length + 1,
+      ...postData,
+    });
+
+    return postData;
+  }
+
+  @Put()
+  update(@Body() postData: PostModel): PostModel {
+    const updatedPost = posts.map((post) =>
+      post.id === postData.id ? postData : post,
+    );
+
+    posts = updatedPost;
+
+    return postData;
+  }
+
+  @Delete(':id')
+  delete(@Param('id') postId: string): PostModel[] {
+    const deletedPosts = posts.filter((post) => post.id !== +postId);
+    posts = deletedPosts;
+
+    return deletedPosts;
   }
 }
