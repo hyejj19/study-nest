c98a59a08f9761cd4876dc71af2afbaa329c1b04
hyejj19@naver.com <hyejj19@naver.com>
2024-02-12 19:30:27 +0900

    엔티티로 테이블 생성하기

--- src/posts/entities/post.entity.ts
+++ src/posts/entities/post.entity.ts
@@ -1,1 +1,22 @@
-export class Post {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class PostsModel {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  author: string;
+
+  @Column()
+  title: string;
+
+  @Column()
+  content: string;
+
+  @Column()
+  likeCount: number;
+
+  @Column()
+  commentCount: number;
+}
