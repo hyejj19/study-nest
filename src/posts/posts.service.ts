8aeffe6253a6a8d21756fa0a74a3500cf382e79c
hyejj19@naver.com <hyejj19@naver.com>
2024-02-12 20:30:33 +0900

    typeORM 을 사용한 DB 연동

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -3,42 +3,6 @@ import { Repository } from 'typeorm';
 import { PostsModel } from './entities/post.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 
-export interface PostModel {
-  id: number;
-  author: string;
-  title: string;
-  content: string;
-  likeCount: number;
-  commentCount: number;
-}
-
-let posts: PostModel[] = [
-  {
-    id: 1,
-    author: 'newjeans_official',
-    title: '뉴진스 민지',
-    content: '민지는 예쁘다',
-    likeCount: 10000000,
-    commentCount: 99999,
-  },
-  {
-    id: 2,
-    author: 'newjeans_official',
-    title: '해린',
-    content: '해린은 이쁘다',
-    likeCount: 10000000,
-    commentCount: 99999,
-  },
-  {
-    id: 3,
-    author: 'newjeans_official',
-    title: '다니엘',
-    content: '다니엘은 이쁘다',
-    likeCount: 10000000,
-    commentCount: 99999,
-  },
-];
-
 @Injectable()
 export class PostsService {
   constructor(
@@ -46,63 +10,65 @@ export class PostsService {
     private readonly postsRepository: Repository<PostsModel>,
   ) {}
 
-  getAllPosts(): PostModel[] {
-    return posts;
+  async getAllPosts() {
+    return this.postsRepository.find();
   }
 
-  getOnePost(postId: number): PostModel {
-    const post = posts.find((post) => post.id === postId);
+  async getOnePost(id: number) {
+    const post = await this.postsRepository.findOne({
+      where: {
+        id,
+      },
+    });
 
-    if (!post) {
-      throw new NotFoundException();
-    }
+    if (!post) throw new NotFoundException();
 
     return post;
   }
 
-  createPost(author: string, title: string, content: string): PostModel {
-    const newPost: PostModel = {
-      id: posts.length + 1,
+  async createPost(author: string, title: string, content: string) {
+    const post = this.postsRepository.create({
       author,
       title,
       content,
-      commentCount: 0,
       likeCount: 0,
-    };
+      commentCount: 0,
+    });
 
-    posts.push(newPost);
+    const newPost = await this.postsRepository.save(post);
 
     return newPost;
   }
 
-  updatePost(
+  async updatePost(
     id: number,
     author?: string,
     title?: string,
     content?: string,
-  ): PostModel {
-    const post = this.getOnePost(id);
+  ) {
+    const post = await this.getOnePost(id);
 
     if (author) {
       post.author = author;
     }
+
     if (title) {
       post.title = title;
     }
+
     if (content) {
       post.content = content;
     }
 
-    posts = posts.map((prev) => (prev.id === +id ? post : prev));
+    const newPost = await this.postsRepository.save(post);
 
-    return post;
+    return newPost;
   }
 
-  deletePost(id: number): number {
-    this.getOnePost(id);
+  async deletePost(id: number) {
+    await this.getOnePost(id);
 
-    const deletedPosts = posts.filter((post) => post.id !== +id);
-    posts = deletedPosts;
+    await this.postsRepository.delete(id);
 
     return id;
   }
